4ccfdd19ebbf35ed58cada0f5a4f31b8e94002e8
Junior Alves <jjalves96@gmail.com>
2021-02-16 08:49:02 -0300

    feat: create Title component

---
+++ src/components/Title/styles.ts
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+export const Title = styled.h2`
+  font-size: 2.4rem;
+  line-height: 2rem;
+  color: #fff;
+  text-transform: uppercase;
+  margin-bottom: 10rem;
+  text-align: center;
+`;
